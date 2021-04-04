e2455394409368757af570d819fb73d493624f77
Ian Godman <32035142+udsl@users.noreply.github.com>
2021-04-04 20:09:05 +0100

    numericalFormatGroup setting lable

---
+++ build.sbt
@@ -0,0 +1,29 @@
+name := "Processor6502"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
+
+idePackagePrefix := Some("com.udsl.processor6502")
+
+// https://mvnrepository.com/artifact/org.scalafx/scalafx
+libraryDependencies += "org.scalafx" %% "scalafx" % "15.0.1-R21"
+
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.1" % "test"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.5"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.5" % "test"
+
+// Add OS specific JavaFX dependencies
+val javafxModules = Seq("base", "controls", "fxml", "graphics", "media", "swing", "web")
+val osName = System.getProperty("os.name") match {
+  case n if n.startsWith("Linux") => "linux"
+  case n if n.startsWith("Mac") => "mac"
+  case n if n.startsWith("Windows") => "win"
+  case _ => throw new Exception("Unknown platform!")
+}
+libraryDependencies ++= javafxModules.map(m => "org.openjfx" % s"javafx-$m" % "15.0.1" classifier osName)
+
+// Fork a new JVM for 'run' and 'test:run', to avoid JavaFX double initialization problems
+fork := true
+
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,147 @@
+package com.udsl.processor6502
+
+import javafx.event.{ActionEvent, EventHandler}
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.collections.ObservableBuffer
+import scalafx.geometry.{Insets, Orientation, Pos}
+import scalafx.scene.Scene
+import scalafx.scene.control._
+import scalafx.scene.layout.{GridPane, _}
+
+
+object Main extends JFXApp {
+
+  stage = new JFXApp.PrimaryStage {
+    title = "6502 Processor"
+    width = 600
+    height = 800
+    resizable = false
+
+    scene = new Scene {
+      root = {
+        // grid1 places the children by specifying the rows and columns in GridPane.setConstraints()
+        val registersBox: VBox = new VBox {
+          val registersCaption: Label = new Label {
+            text = "Processor Registers"
+            padding = Insets(0,0,0,80)
+          }
+
+          val label1: Label = new Label("PC:") {
+            style = "-fx-font-weight:bold"
+            alignmentInParent = Pos.BottomLeft
+          }
+          GridPane.setConstraints(label1, 0, 0, 1, 1)
+
+          val pc: TextField = new TextField {
+//            promptText = "Hi! I am Scalafx TextField"
+            maxWidth = 200
+
+            override def onAction_=(v: EventHandler[ActionEvent]): Unit = super.onAction_=(v)
+          }
+
+          GridPane.setConstraints(pc, 2, 0, 1, 1)
+
+          val registersGrid = new GridPane {
+            hgap = 4
+            vgap = 6
+            margin = Insets(18)
+            children ++= Seq(label1, pc)
+          }
+
+          children ++= Seq(registersCaption, registersGrid)
+        }
+
+        val memoryBox: VBox = new VBox {
+           val memeoryBoxCaption = new Label {
+            text = "Memory View"
+             padding = Insets(0,0,0,80)
+          }
+
+          val seq = Seq("Row 1", "Row 2", "Long Row 3", "Row 4", "Row 5", "Row 6", "Row 7",
+            "Row 8", "Row 9", "Row 10", "Row 11", "Row 12", "Row 13", "Row 14", "Row 15",
+            "Row 16", "Row 17", "Row 18", "Row 19", "Row 20")
+
+          val memoryView = new ListView[String] {
+            items = ObservableBuffer(seq)
+            orientation = Orientation.Vertical
+          }
+
+          val st = new StackPane {
+            padding = Insets(10)
+            children = memoryView
+          }
+
+          children = List(memeoryBoxCaption, st)
+        }
+
+        val numericalFormatGroup = new ToggleGroup()
+
+        val numFormatLabel = new Label()
+
+        val checks: HBox =  new HBox {
+            //Radio Button Toggle Group
+            children = List(
+              new RadioButton {
+                minWidth = 100
+                maxWidth = 200
+                maxHeight = 50
+                text = "Dec"
+                id = text.value
+                toggleGroup = numericalFormatGroup
+                selected = true
+              },
+              new RadioButton {
+                minWidth = 100
+                maxWidth = 200
+                maxHeight = 50
+                text = "Hex"
+                id = text.value
+                toggleGroup = numericalFormatGroup
+              },
+              new RadioButton {
+                minWidth = 100
+                maxWidth = 200
+                maxHeight = 50
+                text = "Oct"
+                id = text.value
+                toggleGroup = numericalFormatGroup
+              },
+              new RadioButton {
+                minWidth = 100
+                maxWidth = 200
+                maxHeight = 50
+                text = "Bin"
+                id = text.value
+                toggleGroup = numericalFormatGroup
+              }
+            )
+        }
+
+        numericalFormatGroup.selectToggle(numericalFormatGroup.toggles(0))
+        numericalFormatGroup.selectedToggle.onChange {
+          val rb = numericalFormatGroup.selectedToggle.get.asInstanceOf[javafx.scene.control.ToggleButton]
+          if (rb != null) numFormatLabel.text = rb.id()
+        }
+
+        val centerLabel = Label( "Number format")
+
+        new BorderPane {
+          maxWidth = 400
+          maxHeight = 300
+          padding = Insets(20)
+          top = new VBox{
+            padding = Insets(20)
+            children = List(Label("Number formats"), checks)
+          }
+          left = registersBox
+          center = numFormatLabel
+          right = memoryBox
+//          bottom = bottomLabel
+        }
+
+      }
+
+    }
+  }
+}
